4494e66ed0b7ab6901bc5d6a5a0ac2342b9bc42d
Fabricio <contact@fabricioguardia.com>
2021-12-04 21:02:03 -0800

    drop files form .gitignore

--- next.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  reactStrictMode: true,
-  publicRuntimeConfig: {
-    APP_NAME: 'Fabworkflow',
-    API: 'http://localhost:3001/api',
-    PRODUCTION: false,
-    DOMAIN: 'http://localhost:3000'
-  }
-}
