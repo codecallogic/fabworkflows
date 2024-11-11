96d9433ca423ac5d303a2c85c95209d3b5f35e4b
Fabricio <contact@fabricioguardia.com>
2024-11-11 15:29:30 -0800

    pm2 ecosystem update

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -13,7 +13,7 @@ module.exports = {
       repo : 'git@github.com:codecallogic/fabworkflows.git',
       path : '/home/ubuntu/client',
       'pre-deploy-local': '',
-      'post-deploy' : 'source ~/.nvm/nvm.sh && npm install --legacy-peer-deps && npm run build && pm2 reload ecosystem.config.js --env production',
+      'post-deploy' : 'source ~/.nvm/nvm.sh && nvm use 14.21.3 && npm install --legacy-peer-deps && npm run build && pm2 reload ecosystem.config.js --env production',
       'pre-setup': '',
       'ssh_options': 'ForwardAgent=yes'
     }
